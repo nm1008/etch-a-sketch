6300f86ad6ef3221f2ab4ee057eaf0d393d53cba
nm1008 <mallari08nikko@yahoo.com>
2023-07-25 10:20:17 +0800

    added section on index.html / made a loop for printing the boxes

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("Test")
+const main = document.querySelector('.main')
+const container = document.querySelector('.container')
+
+for(let i = 0; i < 16; i++){
+    const newDiv = document.createElement('div')
+    newDiv.classList.add('container')
+    main.appendChild(newDiv)
+}
+
