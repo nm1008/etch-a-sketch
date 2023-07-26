9872116680f2f66a5693a8e66ec26d340c26b069
nm1008 <mallari08nikko@yahoo.com>
2023-07-26 11:07:37 +0800

    remodify index.html style.css and script.js

--- script.js
+++ script.js
@@ -1,7 +1,35 @@
 const main = document.querySelector('.main')
 const container = document.querySelector(".container")
-const redButton = document.querySelector(".red")
-const whiteButton = document.querySelector(".white")
+const blackButton = document.querySelector(".black")
+const randomButton = document.querySelector(".random")
+const message = document.querySelector(".info")
+
+const setGrid = document.querySelector(".setGrid")
+const resetButton = document.querySelector(".reset")
+
+function grid(){
+    let squares = main.querySelectorAll(".container");
+    squares.forEach((div) => {
+        div.remove();
+    })
+
+    let newGrid = prompt("how many grids do you want to make?");
+
+    if (newGrid === ""){
+        message.innerHTML = "Please enter a number"
+    } else if (newGrid <= 0 || newGrid > 100){
+        message.innerHTML = "Please enter a number between 1 to 100"
+    } else {
+        message.remove()
+        createGrid(newGrid);
+
+    }
+
+   
+    main.style.gridTemplateColumns = `repeat(${newGrid}, 1fr)`
+    main.style.gridTemplateRows = `repeat(${newGrid}, 1fr)`
+}
+setGrid.addEventListener("click", grid)
 
 function createGrid(size){
 
@@ -12,6 +40,16 @@ function createGrid(size){
     }
 }
 
+function reset(){
+    let squares = main.querySelectorAll(".container") 
+    squares.forEach((div) => {
+        div.style.backgroundColor = "white"
+    })
+}
+resetButton.addEventListener("click", reset)
+
+createGrid(64) //default grid
+
 function changeColor(color){
     const containers = document.querySelectorAll(".container");
     containers.forEach((div) => {
@@ -21,12 +59,15 @@ function changeColor(color){
     });
 }
 
-redButton.addEventListener("click", () => {
-    changeColor("red")
+
+
+blackButton.addEventListener("click", () => {
+    changeColor("black")
 })
 
-whiteButton.addEventListener("click", () => {
-    changeColor("white")
+randomButton.addEventListener("click", () => {
+    let randomColor = `hsl(${Math.random() * 360}. 100%, 50%)`
+    
 })
 
-createGrid(16)
+
